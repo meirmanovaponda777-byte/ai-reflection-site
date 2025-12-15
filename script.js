ad904bea7eb15bd71d995f33c09466132a551128
meirmanovaponda777-byte <meirmanovaponda777@gmail.com>
2025-12-15 18:52:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -41,4 +41,19 @@ analyzeBtn.addEventListener('click', () => {
         result.textContent = "Здесь появится анализ текста (пока заглушка).";
     }
 });
+document.getElementById("analyzeBtn").addEventListener("click", async () => {
+    const text = document.getElementById("userText").value;
+    if (!text) return alert("Введите текст!");
+
+    const response = await fetch("http://127.0.0.1:5000/analyze", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text })
+    });
+
+    const data = await response.json();
+    document.getElementById("result").innerText = data.result;
+});
 
